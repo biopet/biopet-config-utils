8363be77ca61f9c3ff5c10df388d1d5e49e96a33
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-04 10:02:33 +0200

    formatter

--- src/main/scala/nl/biopet/utils/config/Config.scala
+++ src/main/scala/nl/biopet/utils/config/Config.scala
@@ -12,27 +12,40 @@ import scala.io.Source
 case class Config(rootJson: JsObject) {
 
   def samples: Map[String, JsObject] = {
-    (rootJson \ "samples").validate[JsObject].getOrElse(Json.obj())
-      .fields.map(x => x._1 -> x._2.validate[JsObject].getOrElse(Json.obj())).toMap
+    (rootJson \ "samples")
+      .validate[JsObject]
+      .getOrElse(Json.obj())
+      .fields
+      .map(x => x._1 -> x._2.validate[JsObject].getOrElse(Json.obj()))
+      .toMap
   }
 
   def libraries(sample: String): Map[String, JsObject] = {
-    (samples(sample) \ "libraries").validate[JsObject].getOrElse(Json.obj())
-      .fields.map(x => x._1 -> x._2.validate[JsObject].getOrElse(Json.obj())).toMap
+    (samples(sample) \ "libraries")
+      .validate[JsObject]
+      .getOrElse(Json.obj())
+      .fields
+      .map(x => x._1 -> x._2.validate[JsObject].getOrElse(Json.obj()))
+      .toMap
   }
 
   def readgroups(sample: String, library: String): Map[String, JsObject] = {
-    (libraries(sample)(library) \ "readgroups").validate[JsObject].getOrElse(Json.obj())
-      .fields.map(x => x._1 -> x._2.validate[JsObject].getOrElse(Json.obj())).toMap
+    (libraries(sample)(library) \ "readgroups")
+      .validate[JsObject]
+      .getOrElse(Json.obj())
+      .fields
+      .map(x => x._1 -> x._2.validate[JsObject].getOrElse(Json.obj()))
+      .toMap
   }
 
 }
 
 object Config extends Logging {
   def fromFile(file: File): Config = {
-    val value = if (file.getName.endsWith(".json"))
-      Json.parse(Source.fromFile(file).mkString)
-    else loadYaml(file)
+    val value =
+      if (file.getName.endsWith(".json"))
+        Json.parse(Source.fromFile(file).mkString)
+      else loadYaml(file)
 
     value match {
       case o: JsObject => Config(o)
@@ -43,7 +56,6 @@ object Config extends Logging {
   def loadYaml(file: File): JsValue = {
     loadYaml(Source.fromFile(file).mkString)
   }
-
 
   def loadYaml(content: String): JsValue = {
     val yaml = (new Yaml).load(content)
