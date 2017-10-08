de1fa7824a23a0cf6703d122851b76631d1e6d2e
Peter van 't Hof <p.j.van_t_hof@lumc.nl>
2017-10-08 19:37:04 +0200

    Moved methods to Conversions

--- src/main/scala/nl/biopet/utils/config/Conversions.scala
+++ src/main/scala/nl/biopet/utils/config/Conversions.scala
@@ -3,7 +3,7 @@ package nl.biopet.utils.config
 import java.io.{File, PrintWriter}
 
 import org.yaml.snakeyaml.Yaml
-import play.api.libs.json.Json
+import play.api.libs.json._
 
 import scala.collection.JavaConversions._
 
@@ -61,12 +61,39 @@ object Conversions {
   lazy val yaml = new Yaml()
 
   def mapToYaml(map: Map[String, Any]): String =
-    yaml.dump(yaml.load(Json.stringify(Config.mapToJson(map))))
+    yaml.dump(yaml.load(Json.stringify(mapToJson(map))))
 
   def mapToYamlFile(map: Map[String, Any], outputFile: File): Unit = {
     val writer = new PrintWriter(outputFile)
     writer.println(mapToYaml(map))
     writer.close()
   }
+
+  /** Convert native scala map to json */
+  def mapToJson(map: Map[String, Any]): JsObject = {
+    JsObject.apply(map.map(x => x._1 -> anyToJson(x._2)))
+  }
+
+  /** Convert native scala value to json, fall back on .toString if type is not a native scala value */
+  def anyToJson(any: Any): JsValue = {
+    any match {
+      case j: JsValue => j
+      case None => JsNull
+      case Some(x) => anyToJson(x)
+      case m: Map[_, _] => mapToJson(m.map(m => m._1.toString -> anyToJson(m._2)))
+      case l: List[_] => JsArray(l.map(anyToJson))
+      case l: Array[_] => JsArray(l.map(anyToJson))
+      case b: Boolean => JsBoolean(b)
+      case n: Int => JsNumber(n)
+      case n: Double => JsNumber(n)
+      case n: Long => JsNumber(n)
+      case n: Short => JsNumber(n.toInt)
+      case n: Float => JsNumber(n.toDouble)
+      case n: Byte => JsNumber(n.toInt)
+      case null => JsNull
+      case _ => JsString(any.toString)
+    }
+  }
+
 
 }
